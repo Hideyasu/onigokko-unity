37602dd737904da3526de334075bdfb9ba0ea9cb
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-09-14 01:37:09 +0900

    背景画像のインポート

--- unity/Assets/Scripts/ Debug/ScriptableObject/DontDestroyScriptableObject.cs
+++ unity/Assets/Scripts/ Debug/ScriptableObject/DontDestroyScriptableObject.cs
@@ -1,15 +1,15 @@
-using UnityEngine;
-using System.Collections.Generic;
+// using UnityEngine;
+// using System.Collections.Generic;
 
-/// <summary>
-/// シーン変更に関する設定を管理するScriptableObject
-/// </summary>
-namespace UICreate
-{
-    [CreateAssetMenu(fileName = "DontDestroyScriptableObject", menuName = "Scriptable Objects/DontDestroyScriptableObject")]
-    public class DontDestroyScriptableObject : ScriptableObject
-    {
-        [Header("シーンリスト")]
-        public SceneAsset[] SceneLists;
-    }
-}
+// /// <summary>
+// /// シーン変更に関する設定を管理するScriptableObject
+// /// </summary>
+// namespace UICreate
+// {
+//     [CreateAssetMenu(fileName = "DontDestroyScriptableObject", menuName = "Scriptable Objects/DontDestroyScriptableObject")]
+//     public class DontDestroyScriptableObject : ScriptableObject
+//     {
+//         [Header("シーンリスト")]
+//         public SceneAsset[] SceneLists;
+//     }
+// }
