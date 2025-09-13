a04336e06f1b1f71c6af9385fb2e3a4ce4ee3489
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-09-14 07:36:11 +0900

    サウンドマネージャー作成

---
+++ unity/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioSource seSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        if (bgmSource.clip == clip) return;
+
+        bgmSource.clip = clip;
+        bgmSource.loop = true;
+        bgmSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+        bgmSource.clip = null;
+    }
+
+    public void PlaySE(AudioClip clip)
+    {
+        seSource.PlayOneShot(clip);
+    }
+}
